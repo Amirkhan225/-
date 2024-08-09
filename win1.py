9197591b3346fd1cdd6b45b4d681640b6603d5bc
Amirkhan225 <amirkhanisataev@gmail.com>
2024-08-09 13:38:30 +0600

    update

--- win1.py
+++ win1.py
@@ -1,3 +1,10 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton
 from random import randint
-number=randint(1,6)
-print(number)
+app=QApplication([])
+main_win=QWidget()
+main_win.show()
+main_win.setWindowTitle('Кости')
+main_win.move(900, 70)
+main_win.resize(400,200)
+app.exec()
